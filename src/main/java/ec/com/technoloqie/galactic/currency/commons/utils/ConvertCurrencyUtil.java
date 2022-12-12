9e7558de8c1427d09976df3a3a4ed16403765cd3
diego-mac <dijavaji@gmail.com>
2022-12-12 10:17:13 -0500

    agrego comprobacion de simbolos

--- src/main/java/ec/com/technoloqie/galactic/currency/commons/utils/ConvertCurrencyUtil.java
+++ src/main/java/ec/com/technoloqie/galactic/currency/commons/utils/ConvertCurrencyUtil.java
@@ -2,6 +2,7 @@ package ec.com.technoloqie.galactic.currency.commons.utils;
 
 import java.util.HashMap;
 import java.util.Stack;
+import java.util.regex.Pattern;
 
 import ec.com.technoloqie.galactic.currency.commons.CurrencyConstants;
 
@@ -64,11 +65,8 @@ public final class ConvertCurrencyUtil {
 			}
 		
 			// "I" can be subtracted from "V" and "X" only.
-		
 			// "X" can be subtracted from "L" and "C" only.
-		
 			// "C" can be subtracted from "D" and "M" only.
-		
 			// "V","L", and "D" can never be subtracted.
 			//
 			// Only one small-value symbol may be subtracted from any
@@ -76,15 +74,12 @@ public final class ConvertCurrencyUtil {
 		
 			if (nextSymbol != 'H'
 						&& (nextSymbol == CurrencyConstants.ROMAN_I && (currentSymbol == CurrencyConstants.ROMAN_V || currentSymbol == CurrencyConstants.ROMAN_X))
-		
 						|| (nextSymbol == CurrencyConstants.ROMAN_X && (currentSymbol == CurrencyConstants.ROMAN_L || currentSymbol == CurrencyConstants.ROMAN_C))
-		
 						|| (nextSymbol == CurrencyConstants.ROMAN_C && (currentSymbol == CurrencyConstants.ROMAN_D || currentSymbol == CurrencyConstants.ROMAN_M))) {
 		
 					totalUnits += currencyMap.get(currentSymbol) - currencyMap.get(symbolStack.pop());
 		
 				} else {
-		
 					totalUnits += currencyMap.get(currentSymbol);
 				}
 			}
@@ -93,32 +88,65 @@ public final class ConvertCurrencyUtil {
 		return convertUnitsToCredits(totalUnits);
 	}
 	
-	/*
-	 * Coverts units to credit depend on current metal type
+	/**
+	 * Convierte unidades a creditos dependiendo del tipo de metal.
+	 * @param numberOfUnits
+	 * @return
 	 */
 	private double convertUnitsToCredits(int numberOfUnits) {
 
 		// Credit for iron
 		if (CurrencyConstants.currentMetalType == CurrencyConstants.METAL_IRON) {
-
 			return numberOfUnits * CurrencyConstants.IRON_PER_UNIT;
-
 		}
 		// Credit for Silver
 		else if (CurrencyConstants.currentMetalType == CurrencyConstants.METAL_SILVER) {
-
 			return numberOfUnits * CurrencyConstants.SILVER_PER_UNIT;
-
 		}
 		// Credit for Gold
 		else if (CurrencyConstants.currentMetalType == CurrencyConstants.METAL_GOLD) {
-
 			return numberOfUnits * CurrencyConstants.GOLD_PER_UNIT;
-
 		}
 		// just checking credits
 		else {
 			return numberOfUnits;
 		}
+	}
+	
+	/**
+	 * Validacion de sucesion de simbolos romanos 
+	 * @param string
+	 * @return
+	 */
+	public final String checkRepeatedSuccession(String string) {
+
+		String errorMessage = null;
+
+		// "I", "X", "C", and "M" can be repeated three times
+		if (Pattern.compile(CurrencyConstants.REGEX_MORE_THAN_3_OCCURANCE).matcher(string).find()) {
+			errorMessage = CurrencyConstants.ERROR_MESSAGE_4_OCCURANCE;
+		}
+
+		// "D", "L", and "V" can never be repeated
+		if (Pattern.compile(CurrencyConstants.REGEX_MORE_THAN_ONE_OCCURANCE).matcher(string).find()) {
+			errorMessage = CurrencyConstants.ERROR_MESSAGE_2_OCCURANCE;
+		}
+
+		// "I" cannot occure more than once in from of C,X,L,C,D,M
+		if (Pattern.compile(CurrencyConstants.REGEX_MORE_THAN_1_OCCURANCE_OF_I_AS_PREFIX).matcher(string).find()) {
+			errorMessage = CurrencyConstants.ERROR_MESSAGE_2_OCCURANCE_I_PREFIX;
+		}
+
+		// "X" cannot occure more than once in from of L,C,D,M
+		if (Pattern.compile(CurrencyConstants.REGEX_MORE_THAN_1_OCCURANCE_OF_X_AS_PREFIX).matcher(string).find()) {
+			errorMessage = CurrencyConstants.ERROR_MESSAGE_2_OCCURANCE_X_PREFIX;
+		}
+
+		// "C" cannot occure more than once in from of D,M
+		if (Pattern.compile(CurrencyConstants.REGEX_MORE_THAN_1_OCCURANCE_OF_C_AS_PREFIX).matcher(string).find()) {
+			errorMessage = CurrencyConstants.ERROR_MESSAGE_2_OCCURANCE_C_PREFIX;
+		}
+
+		return errorMessage;
 	}
 }

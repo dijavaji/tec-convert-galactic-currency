9945257d13126c6f2e3500a9cac0047c1669ad84
diego-mac <dijavaji@gmail.com>
2022-12-12 20:41:59 -0500

    agrego configuracion de log

--- src/main/java/ec/com/technoloqie/galactic/currency/commons/log/GalacticCurrencyLog.java
+++ src/main/java/ec/com/technoloqie/galactic/currency/commons/log/GalacticCurrencyLog.java
@@ -1,5 +1,6 @@
 package ec.com.technoloqie.galactic.currency.commons.log;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 
@@ -7,17 +8,21 @@ public class GalacticCurrencyLog {
 	static final Logger logger = Logger.getLogger(GalacticCurrencyLog.class);
 	private static final GalacticCurrencyLog INSTANCIA = new GalacticCurrencyLog();
 	
+	private GalacticCurrencyLog(){
+		BasicConfigurator.configure();
+	}
+	
 	public static void info(String parameter){
 		logger.info(parameter);
 	}
 	
 	public void debug(String parameter){
 		logger.debug("This is debug : " + parameter);
-		}
+	}
 		
 	public void warn(String parameter){
 		logger.warn("This is warn : " + parameter);
-		}
+	}
 	
 	public static void error(String parameter){
 		logger.error(parameter);
